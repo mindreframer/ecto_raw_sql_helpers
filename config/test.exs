3e5a8bc1e87c9bc44dcb98b2557b9c657e545016
Roman Heinrich <roman.heinrich@gmail.com>
2024-02-11 19:27:18 +0100

    Chore: fix compilation warning in configs

--- config/test.exs
+++ config/test.exs
@@ -1,27 +1,28 @@
-use Mix.Config
+import Config
 
-config :ecto_raw_sql_helpers, ecto_repos: [
-  EctoRawSQLHelpers.PostgresRepoForTest,
-  EctoRawSQLHelpers.MySQLRepoForTest,
-]
+config :ecto_raw_sql_helpers,
+  ecto_repos: [
+    EctoRawSQLHelpers.PostgresRepoForTest,
+    EctoRawSQLHelpers.MySQLRepoForTest
+  ]
 
 config :ecto_raw_sql_helpers, EctoRawSQLHelpers.PostgresRepoForTest,
-       username: "root",
-       password: "root",
-       database: "root",
-       hostname: "localhost",
-       pool: Ecto.Adapters.SQL.Sandbox,
-       port: 5430,
-       show_sensitive_data_on_connection_error: true,
-       pool_size: 1,
-       types: EctoRawSQLHelpers.Postgrex.CustomUUIDType
+  username: "root",
+  password: "root",
+  database: "root",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  port: 5430,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 1,
+  types: EctoRawSQLHelpers.Postgrex.CustomUUIDType
 
 config :ecto_raw_sql_helpers, EctoRawSQLHelpers.MySQLRepoForTest,
-      username: "root",
-      password: "root",
-      database: "root",
-      hostname: "localhost",
-      pool: Ecto.Adapters.SQL.Sandbox,
-      port: 3300,
-      show_sensitive_data_on_connection_error: true,
-      pool_size: 1
+  username: "root",
+  password: "root",
+  database: "root",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  port: 3300,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 1
